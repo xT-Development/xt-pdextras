750bb16bdb036d9442f82f0bfca4225a63e626e4
xThrasherrr <thrashergamezz@gmail.com>
2023-09-28 20:15:27 -0600

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 -- Debug Configs --
-Config.Debug = true -- Set to true to enable debug mode
+Config.Debug = true
 Config.DebugPoly = false
 
 -- General Configs --
@@ -14,6 +14,7 @@ Config.UseArmory = true
 Config.UseBoatHeliLocations = true
 Config.UseBossMenus = true
 Config.UseEvidence = true
+Config.UseGarages = true
 
 -- Fines Config --
 Config.FineCommand = {
@@ -30,7 +31,8 @@ Config.Webhooks = {
 
 -- Boss Menu Configs --
 Config.BossMenus = {
-    { label = 'PD Management', job = 'police', rank = 4, cert = 'none', coords = vec3(461.42, -986.25, 30.73), radius = 0.4 }
+    { label = 'PD Management', job = 'police', rank = 3, cert = 'none', coords = vec3(461.42, -986.25, 30.73), radius = 0.4 },
+    { label = 'PD Management', job = 'police', rank = 4, cert = 'none', coords = vec3(447.05, -974.0, 30.5), radius = 0.4 }
 }
 
 -- Evidence Rooms Config --
@@ -60,64 +62,8 @@ Config.Certifications = {
     }
 }
 
--- Boats & Helis Config --
-Config.BoatsAndHelis = {
-    {
-        label = 'Mission Row Helipad',
-        job = 'police',
-        spawn = vec4(449.9, -981.46, 44.06, 360.0),
-        class = 15, -- Vehicle Class Allowed
-        ped = {
-            model = 's_m_y_cop_01',
-            scenario = 'WORLD_HUMAN_CLIPBOARD',
-            coords = vec4(463.67, -982.35, 43.69, 92.01),
-            icon = 'fas fa-helicopter'
-        },
-        returnLocations = {
-            {
-                coords = {
-                    vec(455.12957763672, -986.68438720703, 43),
-                    vec(454.99450683594, -975.41595458984, 43),
-                    vec(443.66360473633, -975.45849609375, 43),
-                    vec(443.81823730469, -986.82061767578, 43)
-                },
-                radius = 10
-            }
-        },
-        vehicles = {
-            { name = 'PD Maverick', model = 'polmav', livery = 0, rank = 0, cert = 'none' },
-        },
-    },
-    {
-        label = 'PD Boats Dock',
-        job = 'police',
-        spawn = vec4(-785.92, -1438.53, 0.12, 138.35),
-        class = 14, -- Vehicle Class Allowed
-        ped = {
-            model = 's_m_y_cop_01',
-            scenario = 'WORLD_HUMAN_CLIPBOARD',
-            coords = vec4(-782.71, -1441.87, 1.6, 283.12),
-            icon = 'fas fa-anchor'
-        },
-        returnLocations = {
-            {
-                coords = {
-                    vec(-792.79852294922, -1452.7060546875, -3),
-                    vec(-771.24548339844, -1427.0616455078, -3),
-                    vec(-776.81048583984, -1422.7752685547, -3),
-                    vec(-798.68078613281, -1447.5490722656, -3)
-                },
-                radius = 10
-            }
-        },
-        vehicles = {
-            { name = 'PD Dinghy', model = 'dinghy3', rank = 0, cert = 'none' },
-            { name = 'PD Predator', model = 'predator', rank = 0, cert = 'none' },
-        },
-    },
-}
-
 --------------------------------------------
 
 QBCore = exports['qb-core']:GetCoreObject()
 sharedItems = exports.ox_inventory:Items()
+sharedVehicles = QBCore.Shared.Vehicles
